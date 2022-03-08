700ec49494a68c1ee62420296113cdc22d783b98
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-03-08 18:10:40 +0300

    signin signup backend

---
+++ app.js
@@ -0,0 +1,52 @@
+// imports
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./database");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const { localStrategy } = require("./middleware/passport");
+const { jwtStrategy } = require("./middleware/passport");
+// routes import
+const userRoutes = require("./api/users/routes");
+
+// setup
+const app = express();
+const path = require("path");
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// request print on every request - middlewares
+app.use((req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+});
+
+//   middlewares
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+// routes
+app.use("/media", express.static(path.join(__dirname, "media")));
+app.use("/user", userRoutes);
+
+// Error handiling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+// Path not found handleing
+app.use((req, res, next) => {
+  res.json({ msg: "path was not found" });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+  connectDb();
+});
